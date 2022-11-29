5c7a7312e19a36dc48e478c5e9c7b39bfd05cfac
giokim12 <109334986+giokim12@users.noreply.github.com>
2022-11-30 07:26:51 +0900

    [Silver V] Title: 영화감독 숌, Time: 828 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,19 @@
+n = int(input())
+start = 666
+order = 0
+
+while True:
+    if '666' in str(start):
+        order += 1
+
+    if order == n:
+        print(start)
+        break
+
+    else: start +=1
+
+
+
+
+        
+       
