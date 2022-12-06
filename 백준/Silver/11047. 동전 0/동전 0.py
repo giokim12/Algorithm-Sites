66a319bb1fe07c13158ae60363d9755f4027875d
giokim12 <109334986+giokim12@users.noreply.github.com>
2022-12-06 18:31:31 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -3,17 +3,12 @@ money = []
 for i in range(N):
     temp = int(input())
     money.append(temp)
-# print(money)
 cnt = 0
-while K>0:
+while K:
     for i in range(N-1, -1, -1):
-        if K//money[i] == 0:
-            continue
-        elif K//money[i] > 0:
+        if K//money[i] > 0:
             cnt += K//money[i]
             K = K % money[i]
-
-
 
 print(cnt)
 
